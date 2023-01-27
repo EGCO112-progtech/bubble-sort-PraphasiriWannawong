615f7cca8af987f133d634c6c0aba2cf664af2e6
PraphasiriWanna <123046976+PraphasiriWannawong@users.noreply.github.com>
2023-01-27 10:31:28 +0000

    work2bubblemulloc

--- .ccls-cache/@home@runner@bubble-sort-PraphasiriWannawong/sorting.h
+++ .ccls-cache/@home@runner@bubble-sort-PraphasiriWannawong/sorting.h
@@ -40,13 +40,15 @@ int i,j;
 int sorted;
 // how may pair to compare?
 for(i=0;i<n-1;i++){
-  printf("ROUND %d\n",i);
+  sorted=0;
+  //printf("ROUND %d\n",i);
     for(j=0;j<n-1-i;j++){
       if(a[j]>a[j+1]){
         swap(&a[j],&a[j+1]);
+        sorted=1;
       }
       display(a,n);
-    } 
+    } if(sorted==0) break;
   } 
 }
 
